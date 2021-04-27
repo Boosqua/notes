fcdcea143d94fddfa56e1e3d7a9e453041dfac03
Omar Hernandez <oah232@gmail.com>
2021-04-26 19:56:30 -0700

    create front end template with authroutes, reducers, actions, and components

---
+++ frontend/src/components/app.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { AuthRoute, ProtectedRoute } from '../util/route_util';
+import { Switch } from 'react-router-dom';
+
+const App = () => (
+  <div>
+    Hello World!
+  </div>
+);
+
+export default App;
