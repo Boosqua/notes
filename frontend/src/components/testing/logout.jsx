adf51f6156ad8556624604bdad747b148faec904
Omar Hernandez <oah232@gmail.com>
2021-04-30 18:16:06 -0700

    create collapse sidebar animation styling and button

--- frontend/src/components/testing/logout.jsx
+++ frontend/src/components/testing/logout.jsx
@@ -1,12 +1,34 @@
-import React from "react"
+import React, { useState } from "react"
 import { useDispatch } from "react-redux"
-import {logout} from "../../actions/session_actions"
+import {clearSessionErrors, logout} from "../../actions/session_actions"
+import useStyles from "./styles"
+
 export default function Logout(props){
    const dispatch = useDispatch()
+   const [animate, setAnimate] = useState(false)
+   const [faceL, setFaceL] = useState(true)
+   const styles = useStyles()
    return (
-      <div onClick={(e) => {
-         e.preventDefault()
-         logout()(dispatch)
-      }}>logout<div>Bruh</div></div>
+      <div className={styles.hGrid}>
+         <div className={`${styles.hL}`}>
+            <i class="fas fa-camera"></i>
+         </div>
+         <div className={`${styles.hC}`}>
+            <div className={styles.heButtons} onClick={() => {
+               if(!animate) {
+                  setAnimate(true)
+                  setTimeout(() => setAnimate(false), 1000)
+               }
+               setFaceL(!faceL)
+            }}>
+               <div className={animate ? styles.heSpin: ""}>
+                  <i class={faceL ? "fas fa-chevron-left" : "fas fa-chevron-right"}></i>
+               </div>
+            </div>
+         </div>
+         <div className={`${styles.hR}`}>
+            <i class="fas fa-camera"></i>
+         </div>
+      </div>
    )
 }
---
+++ frontend/src/components/testing/styles.js
@@ -0,0 +1,58 @@
+import {createUseStyles} from 'react-jss'
+
+const styles =  createUseStyles({
+   hGrid: {
+      display: 'flex',
+      position: 'fixed',
+      left: 0,
+      top: 0,
+      flexDirection: 'row',
+      width: "100vw",
+      boxShadow: "0 5px 10px rgba(8, 159, 154, .25)",
+      zIndex: 1,
+   },
+   hL: {
+      padding: 10
+   },
+   heSpin: {
+      animation: '$spin 1s linear',
+   },
+   hR: {
+      background: "red",
+      padding: 10
+   },
+   hC: {
+      borderRight: "2px solid rgba(8, 159, 154, .25)",
+      borderLeft: "2px solid rgba(8, 159, 154, .25)",
+      width: "100%",
+      padding: 10
+   },
+   heButtons: {
+      width: 'fit-content',
+      height: 'fit-content',
+      '&:hover': {
+         cursor: "pointer",
+         animation: '$hecolor 2s linear',
+         animationIterationCount: 'infinite'
+      }
+   },
+   "@keyframes hecolor": {
+      '0%':{
+
+      },
+      '50%':{
+         color: "#e8a97c"
+      },
+      '100%': {
+         color: 'white'
+      }
+   },
+   "@keyframes spin": {
+      '0%': {transform: 'rotate(180deg)'},
+      '33%': {transform: 'rotate(360deg)'},
+      '66%': {transform: 'rotate(540deg)'},
+      '100%': {transform: 'rotate(720deg)'},
+   }
+})
+ /* #e8a97c #e27d5e #c48c9d */
+export default styles;
