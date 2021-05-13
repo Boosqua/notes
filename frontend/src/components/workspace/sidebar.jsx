a8c4e835eaaa0d4909684f62728705bb6c7552d3
Omar Hernandez <oah232@gmail.com>
2021-05-12 17:56:02 -0700

    add home button to sidebar

--- frontend/src/components/workspace/sidebar.jsx
+++ frontend/src/components/workspace/sidebar.jsx
@@ -1,5 +1,6 @@
 import React, {useState} from "react";
 import {useSelector} from "react-redux";
+import {Link} from "react-router-dom"
 
 import useStyles from "./styles"
 import FolderItem from "./folder_item"
@@ -11,16 +12,38 @@ export default function Sidebar({folder, notes}){
    const user = useSelector(state => state.session.user)
    const [show, setShow] = useState(false)
    const [collapse, setCollapse] = useState(false)
-   const classes = useStyles({collapse})
+   const classes = useStyles({collapse, hover: true})
+   const currentNote = useSelector( state => state.util.currentNote)
    return (
       <div className={classes.wSSidebar}>
          <div className={classes.wSHeader}>
             {user.name}
          </div>
+         <div>
+         <Link to="/@me">
+            <div className={classes.folderItem}>
+               <div className={classes.itemIcon } style={{color: "white"}}>
+                  <i class="fas fa-house-user"></i>
+               </div>
+               <div className={classes.itemTitle}>
+                  Home
+               </div>
+            </div>
+         </Link>
+
+         </div>
          <FolderItem folder={folder} open={!collapse} add={true} onClick={()=> setCollapse(!collapse)}/>
          {
             collapse ?
-            null
+            <div>
+               {
+                  currentNote._id ? 
+                  <NoteItem note={currentNote} /> 
+                  : 
+                  null
+               }
+               <Button handleClick={() => setShow(true)} expand={true}>Create New Note</Button>
+            </div>
             :
             <div>
                {notes.map( (note, i) => <NoteItem note={note} key={note._id} />)}
